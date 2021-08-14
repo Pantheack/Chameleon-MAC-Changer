c4a8d5321aafe1d6043676bd103c77eb1531a054
Pantheack <88894565+Pantheack@users.noreply.github.com>
2021-08-14 23:01:46 +0300

    Update chameleon-mac-changer.py

--- chameleon-mac-changer.py
+++ chameleon-mac-changer.py
@@ -5,45 +5,42 @@ import os
 
 os.system("apt-get install figlet")
 os.system("clear")
-os.system("figlet Bukalemun MAC Degistirici")
+os.system("figlet Chameleon  MAC Changer")
 print("""
 Github: https://github.com/Pantheack
 
-Bukalemun MAC Adres Değiştirme Programına Hoş Geldiniz.
+Welcome to the chameleon mac changer.
 
-1) MAC Adresi Rastgele Belirle
-2) MAC Adresi Elle Belirle
-3) MAC Adresi Orijinal Haline Döndür
+1) Set Random MAC Address
+2) Set Manual MAC Address
+3) Restore Original MAC Address
 """)
 
 islemno = input("İslem No Girin: ")
 
 if(islemno=="1"):
-    os.system("sudo ip addr")
-    os.system("sudo ip link set dev eth0 down")
-    os.system( "sudo macchanger -r eth0")
-    os.system("sudo ip link set dev eth0 up")
-    print("\033[92mYeni MAC Adresi Rastgele Belirlendi.")
+        os.system("ifconfig eth0 down")
+		os.system("macchanger -r eth0")
+		os.system("ifconfig eth0 up")
+    print("\033[92mYour Mac address has been randomly assigned.")
 
 
 
 if(islemno=="2"):
-    macadres = input("Yeni MAC Adresini Girin: ")
-    os.system("sudo ip addr")
-    os.system("sudo ip link set dev eth0 down")
-    os.system("sudo macchanger --mac" + macadres + "eth0") 
-    os.system("sudo ip link set dev eth0 up")
-    print("\033[92mYeni MAC Adresi Manuel Şekilde Belirlendi.")
+    macadres = input("Yeni Mac Adresini Giriniz > ")
+		os.system("ifconfig eth0 down")
+		os.system("macchanger --mac "+macadres+" eth0")
+		os.system("ifconfig eth0 up")
+    print("\033[92mYour Mac address has been manually assigned.")
 
 
 
 if(islemno=="3"):
-    os.system("sudo ip addr")
-    os.system("sudo ip link set dev eth0 down")
-    os.system( "sudo macchanger -p eth0")
-    os.system("sudo ip link set dev eth0 up")
-    print("\033[92mMAC Adresi Orijinal Haline Döndürüldü.")
+        os.system("ifconfig eth0 down")
+		os.system("macchanger -p eth0")
+		os.system("ifconfig eth0 up")
+    print("\033[92mMAC Address Restored To Its Original State.")
 
 else:
-      print("Hatalı Tuşlama Yaptınız. Program Yeniden Çalıştırılıyor.")
+      print("You made a wrong keystroke. The program will restart.")
       os.system("python chameleon-mac-changer.py")
